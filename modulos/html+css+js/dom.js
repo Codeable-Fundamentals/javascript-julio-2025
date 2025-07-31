5cfdc7627c816293d60c7440df3c7a41e6984486
Grecia Delgado <greciadm2596@gmail.com>
2025-07-30 19:19:31 -0600

    Update

--- modulos/html+css+js/dom.js
+++ modulos/html+css+js/dom.js
@@ -1,37 +1,41 @@
-// variables y funciones para manejar el DOM:
-var input = document.getElementById("nueva-tarea");
-var buttonAgregar = document.getElementById("agregar");
-var buttonBorrar = document.getElementById("borrar");
-var ul = document.getElementById("tareas");
-function mostrarTareas() {
-  ul.innerHTML = "";
-  var tareas = obtenerTareas();
 
-  for (const tarea of tareas) {
-    var li = document.createElement("li");
-    li.textContent = tarea;
-    ul.append(li);
+function domWrapper() {
+  // variables y funciones para manejar el DOM:
+  var input = document.getElementById("nueva-tarea");
+  var buttonAgregar = document.getElementById("agregar");
+  var buttonBorrar = document.getElementById("borrar");
+  var ul = document.getElementById("tareas");
+  function mostrarTareas() {
+    ul.innerHTML = "";
+    var tareas = APP.obtenerTareas();
+
+    for (const tarea of tareas) {
+      var li = document.createElement("li");
+      li.textContent = tarea;
+      ul.append(li);
+    }
   }
-}
 
-function handleAgregar(_event) {
-  console.log("handleAgregar called: ", _event);
-  var nuevaTarea = input.value;
+  function handleAgregar(_event) {
+    console.log("handleAgregar called: ", _event);
+    var nuevaTarea = input.value;
 
-  console.log("Nueva tarea:", nuevaTarea);
-  if (nuevaTarea === "") return;
+    if (nuevaTarea === "") return;
 
-  agregarTarea(nuevaTarea);
-  input.value = "";
+    APP.agregarTarea(nuevaTarea);
+    input.value = "";
+    mostrarTareas();
+  }
+
+  function handleBorrar(_event) {
+    ul.innerHTML = "";
+    APP.borrarTareas();
+  }
+
+  buttonAgregar.onclick = handleAgregar;
+  buttonBorrar.onclick = handleBorrar;
+
   mostrarTareas();
 }
 
-function handleBorrar(_event) {
-  ul.innerHTML = "";
-  borrarTareas();
-}
-
-buttonAgregar.onclick = handleAgregar;
-buttonBorrar.onclick = handleBorrar;
-
-mostrarTareas();
+domWrapper();
--- modulos/html+css+js/tareas.js
+++ modulos/html+css+js/tareas.js
@@ -1,13 +1,21 @@
-var tareas = ["tarea 1", "tarea 2", "tarea 3", "tarea 4", "tarea 5"];
+function tareasWrapper() {
+  var tareas = ["tarea 1", "tarea 2", "tarea 3", "tarea 4", "tarea 5"];
 
-function agregarTarea(tarea) {
-  tareas.push(tarea);
+  function agregarTarea(tarea) {
+    tareas.push(tarea);
+  }
+
+  function borrarTareas() {
+    tareas = [];
+  }
+
+  function obtenerTareas() {
+    return tareas;
+  }
+
+  APP.agregarTarea = agregarTarea;
+  APP.borrarTareas = borrarTareas;
+  APP.obtenerTareas = obtenerTareas;
 }
 
-function borrarTareas() {
-  tareas = [];
-}
-
-function obtenerTareas() {
-  return tareas;
-}
+tareasWrapper();
